3a63e0e061f69b3b1e33958055c007503fc95e82
chr.papadopoulos <chr.papadopoulos@kaizengaming.com>
2024-12-10 14:36:37 +0200

    added all days

---
+++ 2024/Day3/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n".Split('\n');
+var file = File.ReadAllLines("C:\\code\\temp\\Day3\\input.txt");
+
+
+var regex = new Regex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled);
+long sum = 0; 
+
+foreach (var line in example)
+{
+    var tets = regex.Matches(line);
+    foreach (Match match in tets)
+    {
+        sum += Convert.ToInt64(match.Groups[1].Value) * Convert.ToInt64(match.Groups[2].Value);
+    }
+}
+//part 2
+var regex2 = new Regex(@"mul\((\d+),(\d+)\)|do\(\)|don't\(\)", RegexOptions.Compiled);
+var example2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".Split('\n');
+var enabled = true;
+
+sum = 0; 
+
+foreach (var line in file)
+{
+    var tets = regex2.Matches(line);
+
+    foreach (Match match in tets)
+    {
+        if (match.ToString() == "do()")
+        {
+            enabled = true;
+        }
+        else if (match.ToString() == "don't()")
+        {
+            enabled = false;
+
+        }
+        else
+        {
+            sum += enabled ? Convert.ToInt64(match.Groups[1].Value) * Convert.ToInt64(match.Groups[2].Value) : 0;
+        }
+    }
+}
+;
---
+++ 2024/Day6/Program.cs
@@ -0,0 +1,162 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+
+Console.WriteLine("Hello, World!");
+
+
+var example=@"....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+
+var test = new Matrix(File.ReadAllText("C:\\code\\temp\\Day6\\input.txt"));
+
+test.Move();
+test.Draw();
+Console.WriteLine(test.Touched.Count);
+
+
+class Matrix
+{
+    public Matrix(string input)
+    {
+        var lines = input.Split('\n');
+        MaxX = lines.Length;
+        MaxY = lines.First().Length;
+        
+        for (var index = 0; index < lines.Length; index++)
+        {
+            var line = lines[index];
+            
+            for (var index1 = 0; index1 < line.Length; index1++)
+            {
+                var cha = line[index1];
+                if (cha == '#')
+                {
+                    Obstacles.Add(new Point(index1, index));
+                }
+
+                if (cha is '^' or '>' or '<' or 'v')
+                {
+                    Soldier = new Soldier()
+                    {
+                        Movement = cha switch
+                        {
+                            'v' => Movement.Right,
+                            '<' => Movement.Left,
+                            '^' => Movement.Up,
+                            '>' => Movement.Down,
+                        },
+                        Point = new Point(index1, index)
+                    };
+                }
+            }
+        }
+    }
+    
+    private int MaxX { get; set; }
+    private int MaxY { get; set; }
+    private Soldier Soldier { get; set; }
+    private List<Point> Obstacles { get; set; } = new List<Point>();
+
+    public HashSet<Point> Touched { get; private set; } = new HashSet<Point>();
+
+    public void Draw()
+    {
+        for (int i = 0; i < MaxY; i++)
+        {
+            for (int j = 0; j < MaxX; j++)
+            {
+                if (Obstacles.Any(x => x.X == j && x.Y == i))
+                {
+                    Console.Write('#');
+                }
+                else if(Touched.Any(x => x.X == j && x.Y == i))
+                {
+                    Console.Write('X');
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+    public void Move()
+    {
+        while (true)
+        {
+            if (Soldier.Point.X >= MaxX || Soldier.Point.Y >= MaxY || Soldier.Point.X == 0 || Soldier.Point.Y == 0)
+            {
+                return;
+            }
+
+            if (!WillMoveInRock())
+            {
+                MoveSingle();
+            }
+            else
+            {
+                ChangeDirection();
+                MoveSingle();
+            }
+
+            Touched.Add(Soldier.Point);
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        Soldier.Movement = Soldier.Movement switch
+        {
+            Movement.Up => Movement.Right,
+            Movement.Down => Movement.Left,
+            Movement.Left => Movement.Up,
+            Movement.Right => Movement.Down,
+        };
+    }
+
+    private void MoveSingle()
+    {
+        Soldier.Point = Soldier.Movement switch
+        {
+            Movement.Up => Soldier.Point with { Y = Soldier.Point.Y - 1 },
+            Movement.Down => Soldier.Point with { Y = Soldier.Point.Y + 1 },
+            Movement.Left => Soldier.Point with { X = Soldier.Point.X - 1 },
+            Movement.Right => Soldier.Point with { X = Soldier.Point.X + 1 },
+        };
+    }
+
+    private bool WillMoveInRock()
+    {
+        var nextPoint = Soldier.Movement switch
+        {
+            Movement.Up => Soldier.Point with { Y = Soldier.Point.Y - 1 },
+            Movement.Down => Soldier.Point with { Y = Soldier.Point.Y + 1 },
+            Movement.Left => Soldier.Point with { X = Soldier.Point.X - 1 },
+            Movement.Right => Soldier.Point with { X = Soldier.Point.X + 1 },
+
+        };
+        return Obstacles.Any(x => x.X == nextPoint.X && x.Y == nextPoint.Y);
+    }
+}
+
+class Soldier
+{
+    public Movement Movement { get; set; }
+    public Point Point { get; set; }
+}
+
+enum Movement
+{
+    Up, Down, Left, Right
+}
